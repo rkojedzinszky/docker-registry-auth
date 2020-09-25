30031f4ef58a14c247a40192dd82ca07c4eebda6
Richard Kojedzinszky <richard@kojedz.in>
2020-09-25 15:19:04 +0200

    Added ordering on Admin site

--- dra/admin.py
+++ dra/admin.py
@@ -4,20 +4,24 @@ from dra import forms
 
 @admin.register(models.Group)
 class GroupAdmin(admin.ModelAdmin):
+    ordering = ('name',)
     list_display = ('name',)
 
 @admin.register(models.Account)
 class AccountAdmin(admin.ModelAdmin):
     form = forms.AccountForm
+    ordering = ('username',)
     list_display = ('username',)
     list_filter = ('groups',)
 
 @admin.register(models.Repository)
 class RepositoryAdmin(admin.ModelAdmin):
+    ordering = ('name',)
     list_display = ('name', 'public')
     list_filter = ('public',)
 
 @admin.register(models.RepositoryPermissions)
 class RepositoryPermissionsAdmin(admin.ModelAdmin):
+    ordering = ('repository', 'group')
     list_display = ('repository', 'group', 'write')
     list_filter = ('repository', 'group')
