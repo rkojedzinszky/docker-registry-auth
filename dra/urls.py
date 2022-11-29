ed151c5de57ee2bb0baae315444050071bca131c
Richard Kojedzinszky <richard@kojedz.in>
2022-11-29 17:05:54 +0100

    Switch to django.urls.path

--- dra/urls.py
+++ dra/urls.py
@@ -13,11 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.urls import path
 from django.contrib import admin
 from dra import views
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^token/', views.token),
+    path('admin/', admin.site.urls),
+    path('token/', views.token),
 ]
